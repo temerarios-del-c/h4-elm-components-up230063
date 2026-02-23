e280d9bae220f22544998d531a900f74926379d5
up230063 <up230063@alumnos.upa.edu.mx>
2026-02-22 23:44:30 -0600

    ejercicios

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,7 +1,39 @@
 module Helper exposing (..)
 
-import Html
-import Html.Attributes
+import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
+import Html.Attributes exposing (href)
+
+
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
+
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+headers : String -> Html msg
+headers param =
+    div []
+        [ h1 [] [ text param ]
+        , h2 [] [ text param ]
+        , h3 [] [ text param ]
+        , h4 [] [ text param ]
+        , h5 [] [ text param ]
+        , h6 [] [ text param ]
+        ]
+
+
+hyperlink : String -> String -> Html msg
+hyperlink url label =
+    a [ href url ] [ text label ]
 
 
 
